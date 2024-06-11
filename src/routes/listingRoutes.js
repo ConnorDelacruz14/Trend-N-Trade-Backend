51932c9fafdf05f6345cd5a0d48dedc51c5595b0
Matthew <matthew.duong888@gmail.com>
2024-06-10 18:44:43 -0700

    fixed bug

--- src/routes/listingRoutes.js
+++ src/routes/listingRoutes.js
@@ -4,15 +4,13 @@ const listingController = require('../controllers/listingController');
 
 // Define routes
 
-outer.get('/', listingController.getListings);
+router.get('/', listingController.getListings);
 router.post('/create', listingController.createListing);
 router.put('/editStatus', listingController.updateListingStatus);
 router.post('/getListing', listingController.getListing);
 router.post('/search', listingController.search)
-<<<<<<< HEAD
 router.put('/getListingCheckout', listingController.getListingCheckout);
 router.put('/checkoutListing', listingController.checkoutListing);
-=======
->>>>>>> dd1e8b10e5e98207414b9148dafa3d705c74a213
+
 
 module.exports = router;
