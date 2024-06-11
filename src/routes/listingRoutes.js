95cc7825235c7d405e01db8e2007cb6132941b5e
Connor Delacruz <ConnorD1432@gmail.com>
2024-06-10 19:19:20 -0700

    final

--- src/routes/listingRoutes.js
+++ src/routes/listingRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const listingController = require('../controllers/listingController');
 
 // Define routes
-router.get('/', listingController.getListings);
+router.get('/all', listingController.getListings);
 router.post('/create', listingController.createListing);
 router.put('/editStatus', listingController.updateListingStatus);
 router.post('/getListing', listingController.getListing);
